0d2a489db4e0a1c2be58f49b6eff4be4b74e5319
Carlos Eduardo <kaduppg@gmail.com>
2019-12-10 21:16:05 +1300

    Updates

---
+++ precache-manifest.7114c944161af92beadc8455d56041ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b51029f0045c499792cee8dc79339bb3",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "32590864c2b87c99605f",
+    "url": "/monsters-roledex/static/css/main.0a67e185.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-roledex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "32590864c2b87c99605f",
+    "url": "/monsters-roledex/static/js/main.c8e4544b.chunk.js"
+  },
+  {
+    "revision": "3d698a49286e0395dd3e",
+    "url": "/monsters-roledex/static/js/runtime-main.80f8d167.js"
+  }
+]);
